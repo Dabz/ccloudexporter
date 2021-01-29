19efbdbc679247154bf56a503e25e22f9d733194
Gasparina Damien <d.gasparina@gmail.com>
2021-01-29 17:36:22 +0100

    CLI improved for Metrics API v2

--- cmd/internal/collector/option.go
+++ cmd/internal/collector/option.go
@@ -10,6 +10,7 @@ package collector
 import (
 	"flag"
 	"os"
+	"strings"
 
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
@@ -20,7 +21,9 @@ var supportedGranularity = []string{"PT1M", "PT5M", "PT15M", "PT30M", "PT1H"}
 // ParseOption parses options provided by the CLI and the configuration file
 // This function will panic if the options are invalid
 func ParseOption() {
-	var cluster string
+	var clusters string
+	var connectors string
+	var ksqlApplications string
 	var configPath string
 
 	flag.StringVar(&configPath, "config", "", "Path to configuration file used to override default behavior of ccloudexporter")
@@ -28,7 +31,9 @@ func ParseOption() {
 	flag.StringVar(&Context.HTTPBaseURL, "endpoint", "https://api.telemetry.confluent.cloud/", "Base URL for the Metric API")
 	flag.StringVar(&Context.Granularity, "granularity", "PT1M", "Granularity for the metrics query, by default set to 1 minutes")
 	flag.IntVar(&Context.Delay, "delay", 120, "Delay, in seconds, to fetch the metrics. By default set to 120, this, in order to avoid temporary data points.")
-	flag.StringVar(&cluster, "cluster", "", "Cluster ID to fetch metric for. If not specified, the environment variable CCLOUD_CLUSTER will be used")
+	flag.StringVar(&clusters, "cluster", "", "Comma separated list of cluster ID to fetch metric for. If not specified, the environment variable CCLOUD_CLUSTER will be used")
+	flag.StringVar(&connectors, "connector", "", "Comma separated list of connector ID to fetch metric for. If not specified, the environment variable CCLOUD_CONNECTOR will be used")
+	flag.StringVar(&ksqlApplications, "ksqlDB", "", "Comma separated list of ksqlDB application to fetch metric for. If not specified, the environment variable CCLOUD_KSQL will be used")
 	flag.StringVar(&Context.Listener, "listener", ":2112", "Listener for the HTTP interface")
 	flag.BoolVar(&Context.NoTimestamp, "no-timestamp", false, "Do not propagate the timestamp from the the metrics API to prometheus")
 	versionFlag := flag.Bool("version", false, "Print the current version and exit")
@@ -50,17 +55,18 @@ func ParseOption() {
 		os.Exit(0)
 	}
 
-	if cluster == "" {
-		clusterEnv, present := os.LookupEnv("CCLOUD_CLUSTER")
-		if present {
-			cluster = clusterEnv
-		}
-	}
+	clusters = getFromEnvIfEmpty(clusters, "CCLOUD_CLUSTER")
+	connectors = getFromEnvIfEmpty(connectors, "CCLOUD_CONNECTOR")
+	ksqlApplications = getFromEnvIfEmpty(ksqlApplications, "CCLOUD_KSQL")
 
 	if configPath != "" {
 		parseConfigFile(configPath)
 	} else {
-		createDefaultRule(cluster)
+		createDefaultRule(
+			splitEnv(clusters),
+			splitEnv(connectors),
+			splitEnv(ksqlApplications),
+		)
 	}
 	validateConfiguration()
 }
@@ -106,8 +112,8 @@ func validateConfiguration() {
 	}
 
 	for _, rule := range Context.Rules {
-		if len(rule.Clusters) == 0 {
-			log.Errorln("No cluster ID has been specified in a rule")
+		if len(rule.Clusters) == 0 && len(rule.Connectors) == 0 && len(rule.Ksql) == 0 {
+			log.Errorln("No cluster, connector, or ksqlDB ID has been specified in a rule")
 			flag.Usage()
 			os.Exit(1)
 		}
@@ -150,11 +156,13 @@ func parseConfigFile(configPath string) {
 	}
 }
 
-func createDefaultRule(cluster string) {
+func createDefaultRule(clusters []string, connectors []string, ksqlDBApplications []string) {
 	Context.Rules = make([]Rule, 1)
 	Context.Rules[0] = Rule{
 		id:            0,
-		Clusters:      []string{cluster},
+		Clusters:      clusters,
+		Connectors:    connectors,
+		Ksql:          ksqlDBApplications,
 		Metrics:       DefaultMetrics,
 		GroupByLabels: DefaultGroupingLabels,
 	}
@@ -170,6 +178,23 @@ func upgradeRuleIfRequired(rule Rule) Rule {
 	}
 
 	return rule
+}
+
+func getFromEnvIfEmpty(va string, env string) string {
+	if va == "" {
+		clusterEnv, present := os.LookupEnv(env)
+		if present {
+			return clusterEnv
+		}
+	}
+	return va
+}
+
+func splitEnv(va string) []string {
+	if "" == va {
+		return []string{}
+	}
+	return strings.Split(va, ",")
 }
 
 func setStringIfExit(destination *string, key string) {
